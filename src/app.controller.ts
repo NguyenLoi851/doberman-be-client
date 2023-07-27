23e9b623bcbce8d894fed11381d0986885d900f2
NguyenLoi851 <nguyensyloi851@gmail.com>
2023-07-27 22:45:49 +0700

    verify ssl file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, StreamableFile } from '@nestjs/common';
 import { AppService } from './app.service';
+import { createReadStream } from 'fs';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/.well-known/pki-validation/A754A961C7F6DB7A9A1CB83D0A387D2B.txt')
+  getVerifySslFile() {
+    const file = createReadStream(join(process.cwd(), 'A754A961C7F6DB7A9A1CB83D0A387D2B.txt'));
+    return new StreamableFile(file);
   }
 }
