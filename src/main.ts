c1181eab29fcabdd2fe2e38891e45f31c256a5b1
NguyenLoi851 <nguyensyloi851@gmail.com>
2023-07-27 16:11:04 +0700

    revert to 5907f9e

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as fs from 'fs'
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync(process.env.KEY_CA_PRIVATE_KEY_FILE_PATH),
-    cert: fs.readFileSync(process.env.KEY_CA_PUBLIC_CERTIFICATE_FILE_PATH),
-  };
-
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions
-  });
+  const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('Doberman swagger')
